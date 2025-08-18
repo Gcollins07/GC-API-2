360a4a3e33e9b4c863eda3262058c73c49d0851b
Gcollins07 <garrett.collins5@gmail.com>
2025-08-18 18:27:32 -0400

    early days

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // `http://www.omdbapi.com/?apikey=[yourkey]&`
 // `http://img.omdbapi.com/?apikey=[yourkey]&`
 
+const btn = document.getElementById("search__btn");
+    const clapper = document.getElementById("clapper");
+
+    btn.addEventListener("click", () => {
+      clapper.classList.add("active");
+
+      // After animation, go to new page
+      setTimeout(() => {
+        window.location.href = "https://movies.html"; 
+      }, 900); // slightly longer than animation
+    });
